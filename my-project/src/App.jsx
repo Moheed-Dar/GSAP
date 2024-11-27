1f3444995857bdbe20794d93201c295da743c7ac
Moheed-Dar <moheeddar324@gmail.com>
2024-11-27 14:21:49 +0500

    gsap 3

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -8,7 +8,34 @@ function App ()  {
  const boxref =useRef()
  const boxref2 =useRef()
  const nameref1 =useRef()
+ const tox =useRef()
+ const tox1 =useRef()
+ const tox2 =useRef()
  useGSAP(()=>{
+  gsap.to(tox.current,{
+    x:1000,
+    backgroundColor:"blue",
+    duration:1.5,
+    delay:1,
+    repeat:-1,
+    yoyo:true,
+  })
+  gsap.to(tox1.current,{
+    x:800,
+    backgroundColor:"orange",
+    duration:1.5,
+    delay:1.5,
+    repeat:-1,
+    yoyo:true,
+  })
+  gsap.to(tox2.current,{
+    x:600,
+    backgroundColor:"red",
+    duration:1.5,
+    delay:2.5,
+    repeat:-1,
+    yoyo:true,
+  })
   gsap.from(boxref.current,{
       x:1000,
       y:300,
@@ -56,6 +83,9 @@ function App ()  {
       <h1 ref={nameref1} id="name1" className="text-4xl text-blue-400 " >Moheed</h1><br/>
       <h1 ref={nameref1} id="name1" className="text-5xl text-blue-400 " >Software </h1><br/>
       <h1 ref={nameref1} id="name1" className="text-6xl text-blue-400 " >Developer</h1>
+      <div ref={tox} className="tox h-40 w-40 bg-red-600"></div>
+      <div ref={tox1} className="to1 h-40 w-40 bg-green-600"></div>
+      <div ref={tox2} className="to2 h-40 w-40 bg-yellow-600"></div>
     </div>
   );
 }
