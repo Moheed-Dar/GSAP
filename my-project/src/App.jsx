d42b30e5d22614007e9bfb9a0df42fb0d5096bd8
Moheed-Dar <moheeddar324@gmail.com>
2024-11-27 15:45:54 +0500

    gsap 4

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,91 +1,95 @@
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 // gsap.registerPlugin(ScrollTrigger);
 import { gsap } from "gsap";
-import {useGSAP} from '@gsap/react';
+import { useGSAP } from '@gsap/react';
 import "./App.css";
+import Nav from './component/Nav.jsx'
 import { useRef } from "react";
-function App ()  {
- const boxref =useRef()
- const boxref2 =useRef()
- const nameref1 =useRef()
- const tox =useRef()
- const tox1 =useRef()
- const tox2 =useRef()
- useGSAP(()=>{
-  gsap.to(tox.current,{
-    x:1000,
-    backgroundColor:"blue",
-    duration:1.5,
-    delay:1,
-    repeat:-1,
-    yoyo:true,
+function App() {
+  const boxref = useRef()
+  const boxref2 = useRef()
+  const nameref1 = useRef()
+  const tox = useRef()
+  const tox1 = useRef()
+  const tox2 = useRef()
+  useGSAP(() => {
+    gsap.to(tox.current, {
+      x: 1000,
+      backgroundColor: "blue",
+      duration: 1.5,
+      delay: 1,
+      repeat: -1,
+      yoyo: true,
+    })
+    gsap.to(tox1.current, {
+      x: 800,
+      backgroundColor: "orange",
+      duration: 1.5,
+      delay: 1.5,
+      repeat: -1,
+      yoyo: true,
+    })
+    gsap.to(tox2.current, {
+      x: 600,
+      backgroundColor: "red",
+      duration: 1.5,
+      delay: 2.5,
+      repeat: -1,
+      yoyo: true,
+    })
+    gsap.from(boxref.current, {
+      x: 1000,
+      y: 300,
+      duration: 3,
+      delay: 1,
+      backgroundColor: "yellow",
+      borderRadius: "50%",
+      rotate: 360,
+      width: "30px",
+      height: "30px",
+      repeat: -1,
+      yoyo: true,
+    })
+    gsap.to(boxref2.current, {
+      x: 1000,
+      y: -100,
+      duration: 3,
+      rotate: 360,
+      delay: 1,
+      backgroundColor: "yellow",
+      borderRadius: "50%",
+      width: "30px",
+      height: "30px",
+      repeat: -1,
+      yoyo: true,
+    })
+    gsap.from("#name1", {
+      opacity: 0,
+      y: -20,
+      borderRadius: "50%",
+      duration: 2,
+      delay: 1,
+      stagger: 1,
+      color: "yellow",
+      repeat: -1,
+      yoyo: true,
+    })
   })
-  gsap.to(tox1.current,{
-    x:800,
-    backgroundColor:"orange",
-    duration:1.5,
-    delay:1.5,
-    repeat:-1,
-    yoyo:true,
-  })
-  gsap.to(tox2.current,{
-    x:600,
-    backgroundColor:"red",
-    duration:1.5,
-    delay:2.5,
-    repeat:-1,
-    yoyo:true,
-  })
-  gsap.from(boxref.current,{
-      x:1000,
-      y:300,
-      duration:3,
-      delay:1,
-      backgroundColor:"yellow",
-      borderRadius:"50%",
-      rotate:360,
-      width:"30px",
-      height:"30px",
-      repeat:-1,
-      yoyo:true,
-  })
-  gsap.to(boxref2.current,{
-      x:1000,
-      y:-100,
-      duration:3,
-      rotate:360,
-      delay:1,
-      backgroundColor:"yellow",
-      borderRadius:"50%",
-      width:"30px",
-      height:"30px",
-      repeat:-1,
-      yoyo:true,
-  })
-  gsap.from("#name1",{
-    opacity:0,
-    y:-20,
-    borderRadius:"50%",
-    duration:2,
-    delay:1,
-    stagger:1,
-    color:"yellow",
-    repeat:-1,
-    yoyo:true,  
-  })
- })
-  
+
   return (
     <div className="App" >
+
       <div ref={boxref} className="box mt-10 ms-10 rounded-lg w-72 h-72 bg-red-400">
       </div>
-      <div  ref={boxref2} className="box1 mt-10 ms-10 h-72 w-72 bg-green-600 "></div>
-      <h1 ref={nameref1} id="name1" className="text-4xl text-blue-400 " >Moheed</h1><br/>
-      <h1 ref={nameref1} id="name1" className="text-5xl text-blue-400 " >Software </h1><br/>
+      <div ref={boxref2} className="box1 mt-10 ms-10 h-72 w-72 bg-green-600 "></div>
+      <h1 ref={nameref1} id="name1" className="text-4xl text-blue-400 " >Moheed</h1><br />
+      <h1 ref={nameref1} id="name1" className="text-5xl text-blue-400 " >Software </h1><br />
       <h1 ref={nameref1} id="name1" className="text-6xl text-blue-400 " >Developer</h1>
       <div ref={tox} className="tox h-40 w-40 bg-red-600"></div>
       <div ref={tox1} className="to1 h-40 w-40 bg-green-600"></div>
-      <div ref={tox2} className="to2 h-40 w-40 bg-yellow-600"></div>
+      <div ref={tox2} className="to2 h-40 w-40 bg-yellow-600"></div><br />
+      <br />
+      <Nav />
     </div>
   );
 }
--- my-project/src/component/Nav.jsx
+++ my-project/src/component/Nav.jsx
@@ -1,10 +1,50 @@
-
-import { useEffect, useRef } from "react";
-
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react';
+import { useRef } from "react";
+import { Timeline } from "gsap/gsap-core";
 function Nav() {
-    return (
-      <div>Moheed</div>
-    )
+  const newref = useRef()
+  const newref2 =useRef()
+  const newref3 =useRef()
+  var tl = gsap.timeline()
+  useGSAP(() => {
+
+    tl.from(newref.current, {
+      x:1000,
+      duration:2,
+      delay:1,
+      backgroundColor:"green",
+      repeat:-1,
+      yoyo:true,
+      borderRadius:"50%",
+    })
+    tl.from(newref2.current, {
+      x:1000,
+      duration:2,
+      delay:1,
+      backgroundColor:"green",
+      repeat:-1,
+      yoyo:true,
+      borderRadius:"50%",
+    })
+    tl.from(newref3.current, {
+      x:1000,
+      duration:2,
+      delay:1,
+      backgroundColor:"green",
+      repeat:-1,
+      yoyo:true,
+      borderRadius:"50%",
+    })
+  })
+  return (
+    <div className="Nav" >
+      <div ref={newref} className="h-44 w-44 bg-yellow-500"></div>
+      <div ref={newref2} className="h-44 w-44 bg-yellow-500"></div>
+      <div ref={newref3} className="h-44 w-44 bg-yellow-500">
+      </div>
+    </div>
+  )
 }
 
 export default Nav;
