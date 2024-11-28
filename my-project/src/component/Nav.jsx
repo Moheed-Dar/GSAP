cd1e345e66656457fa8e4a9cda83937f5c697a58
Moheed-Dar <moheeddar324@gmail.com>
2024-11-28 17:15:04 +0500

    complete Scrolltrigger

--- my-project/src/component/Nav.jsx
+++ my-project/src/component/Nav.jsx
@@ -6,66 +6,29 @@ gsap.registerPlugin(ScrollTrigger);
 
 function Nav() {
   useGSAP(()=>{
-    gsap.from(".page1 .box",{
-      scale:0,
-      delay:2,
-      duration:3,
-      rotate:360,
-    })
-    gsap.from(".page2 .box",{
-      scale:0,
-      duration:3,
-      rotate:360,
+    gsap.to(".page2 .h1",{
+      transform:"translateX(-310%)",
       scrollTrigger:{
-         trigger:".page2 .box",
-         scroller:"body",
-         markers:true,
-         start:"top 60%",
-         end:"bottom 50%",
-         scrub:2,
-      }
-    })
-    gsap.from(".page3 h1",{
-      opacity:0,
-      duration:2,
-      x:500,
-      scrollTrigger:{
-        trigger:".page3 h2",
+        trigger:".page2 ",
         scroller:"body",
         markers:true,
-        start:"top 60%",
-        end:"top 50%",
-        scrub:3,
-      }
-    })
-    gsap.from(".page3 h2",{
-      opacity:0,
-      duration:2,
-      x:-500,
-      scrollTrigger:{
-        trigger:".page3 h2",
-        scroller:"body",
-        markers:true,
-        start:"top 60%",
-        end:"top 50%",
+        start:"top 0%",
+        end:"top -100%",
         scrub:3,
         pin:true,
+
       }
     })
+   
   })
   return (
     <div>
       <div className="page1 flex items-center justify-center">
-        <div className="box  h-72 w-72 bg-fuchsia-600"></div>
       </div>
-      <div className="page2 flex items-center justify-center">
-      <div className="box h-72 w-72 bg-green-600"></div>
+      <div className="page2 ">
+        <h1 className="h1" >Moheed-developer</h1>
       </div>
-      <div className="page3 flex items-center pt-20 flex-col gap-10">
-        <h1 className="text-6xl text-orange-300 bg-black w-50 h-30" >hello</h1>
-        <h2 className="text-6xl text-orange-300 bg-black w-70 h-30" >Moheed</h2>
-      </div>
-      <div className="page4"></div>
+      <div className="page3"></div>
     </div>
   )
 }
