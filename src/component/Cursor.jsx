64b36aa574970524962e888f53d8ff0902a0e0a0
Moheed-Dar <moheeddar324@gmail.com>
2024-12-04 17:23:23 +0500

    nav with pump complete

--- src/component/Cursor.jsx
+++ src/component/Cursor.jsx
@@ -1,38 +1,26 @@
 import React, { useEffect } from 'react'
 import { gsap } from "gsap";
-
-
-const Cursor =()=>{
-    useEffect (()=>{
-        const handleMouseMove=(event)=>{
-            const {clientX,clientY} = event;
-            gsap.to("#cursor",{
-                x:clientX-30/2,
-                y:clientY-30/2,
-                duration:1,
-                delay:0,
-                ease:"power4.out"
-            });
-        };
-        const changeColor = () => {
-            gsap.to("#cursor", {
-              backgroundColor: `hsl(${Math.random() * 360}, 100%, 50%)`,
-              duration: 1,
-              repeat: -1,
-              yoyo: true,
-            });
-        };
-        window.addEventListener("mousemove",handleMouseMove);
-        return ()=>{
-            window.removeEventListener("mousemove",handleMouseMove);
-        };
-    },[]);
-
-
-
+const Cursor = () => {
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      gsap.to("#cursor", {
+        x: clientX - 20 / 2,
+        y: clientY - 20 / 2,
+        duration: 1,
+        delay: 0,
+        ease: "power4.out"
+      });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
   return (
-    <div id="cursor" className='h-[30px] w-[30px] bg-green-500 rounded-full fixed top-0 left-0  z-10 ' ></div>
+    <>
+      <div id="cursor" className='h-[22px] w-[22px] bg-white rounded-full fixed top-0 left-0  z-10 pointer-events-none mix-blend-difference ' ></div>
+    </>
   )
 }
-
 export default Cursor
